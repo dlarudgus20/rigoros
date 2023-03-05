db955fb37ae3c5f1a61cb96ebe9d6e307e01f00e
dlarudgus20 <dlarudgus20@naver.com>
2023-03-05 10:41:19 +0900

    irq_mutex ManuallyDrop

--- kernel/src/irq_mutex.rs
+++ kernel/src/irq_mutex.rs
@@ -1,14 +1,40 @@
 use core::ops::{Deref, DerefMut};
+use core::mem::ManuallyDrop;
 use spin::{Mutex, MutexGuard};
 use x86_64::instructions::interrupts::{are_enabled, enable, disable};
+
+struct InterruptGuard {
+    saved_intpt_flag: bool,
+}
 
 pub struct IrqMutex<T> {
     inner: Mutex<T>,
 }
 
 pub struct IrqMutexGuard<'a, T> {
-    inner: MutexGuard<'a, T>,
-    saved_intpt_flag: bool,
+    inner: ManuallyDrop<MutexGuard<'a, T>>,
+    intr: ManuallyDrop<InterruptGuard>,
+}
+
+impl InterruptGuard {
+    fn lock() -> Self {
+        let saved_intpt_flag = are_enabled();
+        if saved_intpt_flag {
+            disable();
+        }
+
+        Self {
+            saved_intpt_flag
+        }
+    }
+}
+
+impl Drop for InterruptGuard {
+    fn drop(&mut self) {
+        if self.saved_intpt_flag {
+            enable();
+        }
+    }
 }
 
 impl<T> IrqMutex<T> {
@@ -19,33 +45,24 @@ impl<T> IrqMutex<T> {
     }
 
     pub fn lock(&self) -> IrqMutexGuard<T> {
-        let saved_intpt_flag = are_enabled();
-        if saved_intpt_flag {
-            disable();
-        }
-
+        let intr = InterruptGuard::lock();
+        let inner = self.inner.lock();
         IrqMutexGuard {
-            inner: self.inner.lock(),
-            saved_intpt_flag,
+            inner: ManuallyDrop::new(inner),
+            intr: ManuallyDrop::new(intr),
         }
     }
 
     pub fn try_lock(&self) -> Option<IrqMutexGuard<T>> {
-        let saved_intpt_flag = are_enabled();
-        if saved_intpt_flag {
-            disable();
-        }
+        let intr = InterruptGuard::lock();
 
         if let Some(guard) = self.inner.try_lock() {
             Some(IrqMutexGuard {
-                inner: guard,
-                saved_intpt_flag,
+                inner: ManuallyDrop::new(guard),
+                intr: ManuallyDrop::new(intr),
             })
         }
         else {
-            if saved_intpt_flag {
-                enable();
-            }
             None
         }
     }
@@ -53,8 +70,9 @@ impl<T> IrqMutex<T> {
 
 impl<T> Drop for IrqMutexGuard<'_, T> {
     fn drop(&mut self) {
-        if self.saved_intpt_flag {
-            enable();
+        unsafe {
+            ManuallyDrop::drop(&mut self.inner);
+            ManuallyDrop::drop(&mut self.intr);
         }
     }
 }
